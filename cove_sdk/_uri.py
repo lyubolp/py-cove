1e324584c4d5da4bac3924f2deab9700c766b65d
Lyuboslav Karev <lyubo.karev@gmail.com>
2026-06-20 11:13:07 +0300

    Add URI handling

---
+++ cove_sdk/_uri.py
@@ -0,0 +1,33 @@
+from enum import StrEnum
+from urllib.parse import urlsplit
+
+
+class ResourceType(StrEnum):
+    JSON_ITEM = "json_item"
+    KEY_VALUE = "key_value"
+    PYTHON_ITEM = "python_item"
+
+
+class URIParseError(Exception):
+    pass
+
+
+def parse_uri(uri: str) -> tuple[str, ResourceType, str, str]:
+    split_result = urlsplit(uri)
+
+    if split_result.scheme != "cove":
+        raise URIParseError(f"Invalid scheme: {split_result.scheme}")
+
+    netloc = split_result.netloc
+
+    path = split_result.path
+
+    try:
+        resource, project_id, key = path.strip("/").split("/")
+    except ValueError as exc:
+        raise URIParseError(f"Invalid path: {path}") from exc
+
+    if resource not in ResourceType.__members__.values():
+        raise URIParseError(f"Invalid resource type: {resource}")
+
+    return netloc, ResourceType(resource), project_id, key
